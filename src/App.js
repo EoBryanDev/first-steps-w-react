b82649e8d7371a92ca1806ee812acd6f65a17fc4
Bryan <mauriciobryan1998@gmail.com>
2022-05-10 19:34:39 -0300

    First Steps in React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+//Class component returning JSX
+class App extends Component{
+
+    state = {
+      name : 'Maurício Bryan',
+      counter : 0
+    };
+
+  // ON CLICK CHANGE THE NAME STATE
+  handlePClick = () => {
+    //const { name } = this.state;
+    //console.log(`<p> foi clicado por ${name}`)
+    this.setState({ name : 'Bryan Please' })
+  }
+  // ON CLICK CHANGE COUNTER
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter : counter + 1})
+  }
+
+  render(){
+    //const name = this.state.name;
+    const { name , counter} = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            {name}
+          </p>
+          <a onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Este é o link clicado {counter} vez(es)
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+//Function Component returning JSX
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
         <a
           className="App-link"
@@ -20,6 +68,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
