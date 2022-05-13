f23cc49b381ca0b6496ed5b422324a0318b59766
Bryan <mauriciobryan1998@gmail.com>
2022-05-13 10:05:29 -0300

    Migrating Code to React Hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import './styles.css';
 
@@ -7,116 +7,80 @@ import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-//Class component returning JSX
-class Home extends Component {
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(2);
+  const [searchValue, setSearchValue] = useState('');
 
-  state = {
-    counter: 0,
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 2,
-    searchValue: ''
-  };
 
-  //<LIFECYCLE METHODS = LIFECYCLE>
-  async componentDidMount() {            //COMPONENT BUILT
-    await this.loadPosts();
-  }
-  //</LIFECYCLE METHODS>
-
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    })
-  }
 
-  loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, [])
 
-    } = this.state
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
 
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage })
+
+    setPosts(posts);
+    setPage(nextPage);
   }
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
+
   }
-  render() {
-    //const name = this.state.name;
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    const filteredPosts = !!searchValue ?
-      allPosts.filter(post => {
-        return post.title.toLowerCase()
-          .includes(searchValue.toLowerCase());
-      })
-      : posts;
+  
+  const noMorePosts = page + postsPerPage >= allPosts.length;
 
-    return (
-      <section className='container'>
-        <div className='search-container'>
-          {!!searchValue && (
-            <h3>Search value: {searchValue}</h3>
-          )}
-          <TextInput
-            searchValue={searchValue}
-            handleChange={this.handleChange}
-          />
-        </div>
-        {filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
-        )}
+  const filteredPosts = !!searchValue ?
+    allPosts.filter(post => {
+      return post.title.toLowerCase()
+        .includes(searchValue.toLowerCase());
+    })
+    : posts;
 
-        {filteredPosts.length === 0 && (
-          <p>There were not found any post!</p>
-        )}
-
-        <div className='button-container'>
-          {!searchValue && (
-            <Button
-              text="Load More posts"
-              onClick={this.loadMorePosts}
-              disabled={noMorePosts}
-            />
-          )}
-
-        </div>
-      </section>
-    );
-  }
-}
-//Function Component returning JSX
-
-/*function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container'>
+      <div className='search-container'>
+        {!!searchValue && (
+          <h3>Search value: {searchValue}</h3>
+        )}
+        <TextInput
+          searchValue={searchValue}
+          handleChange={handleChange}
+        />
+      </div>
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>There were not found any post!</p>
+      )}
+
+      <div className='button-container'>
+        {!searchValue && (
+          <Button
+            text="Load More posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
+        )}
+
+      </div>
+    </section>
   );
-}*/
+}
 
 export default Home;
